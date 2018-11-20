cae8376fb4e48f6ff842e5eeeabe1d95d298b3d5
zeljko todorovic <zeljkotodorovic970@gmail.com>
2018-11-20 13:26:35 +0100

    change images

--- src/main/java/com/zeljko/earthquakes/controller/QuakeController.java
+++ src/main/java/com/zeljko/earthquakes/controller/QuakeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,31 +20,47 @@ public class QuakeController {
         this.quakeService = quakeService;
     }
 
-    @RequestMapping("/quakes")
+    @RequestMapping("/quakesAll")
     public String list(Model model) {
         List<Quake> quakes = quakeService.getAllQuakes();
+        List<Double> mag = new ArrayList<>();
+        mag.add(5.0);
+        mag.add(9.0);
         model.addAttribute("quakes", quakes);
+        model.addAttribute("mag", mag);
         return "quake_list";
     }
 
     @RequestMapping("/quakes5")
     public String quakes4(Model model) {
         List<Quake> quakes = quakeService.getAllByMagBetween(5.0, 5.49);
+        List<Double> mag = new ArrayList<>();
+        mag.add(5.0);
+        mag.add(5.5);
         model.addAttribute("quakes", quakes);
+        model.addAttribute("mag", mag);
         return "quake_list";
     }
 
     @RequestMapping("/quakes5.5")
     public String quakes5(Model model) {
         List<Quake> quakes = quakeService.getAllByMagBetween(5.5, 6.0);
+        List<Double> mag = new ArrayList<>();
+        mag.add(5.5);
+        mag.add(6.0);
         model.addAttribute("quakes", quakes);
+        model.addAttribute("mag", mag);
         return "quake_list";
     }
 
     @RequestMapping("/quakes6")
     public String quakes6(Model model) {
-        List<Quake> quakes = quakeService.getAllByMagGreaterThan(6.0);
+        List<Quake> quakes = quakeService.getAllByMagBetween(6.0, 9.0);
+        List<Double> mag = new ArrayList<>();
+        mag.add(6.0);
+        mag.add(9.0);
         model.addAttribute("quakes", quakes);
+        model.addAttribute("mag", mag);
         return "quake_list";
     }
 
